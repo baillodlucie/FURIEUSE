3e4f7ad5c7b837a28ddf84da88daa21ed2527d17
baillodlucie <122967677+baillodlucie@users.noreply.github.com>
2024-11-13 11:50:08 +0100

    création du footer, début des animations

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,35 @@
+import { gsap } from "gsap";
+
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.to(".container", {
-  xPercent: -67,
-  ease: "none",
+gsap.from(".box-1", {
+  left: "0",
+  opacity: 0,
   scrollTrigger: {
-    trigger: ".horizontal-scroll",
-    start: "top top",
-    end: "+=300%",
-    scrub: 1,
-    pin: true,
+    trigger: ".box-1",
+    start: "top bottom",
+    scrub: true,
+  },
+});
+
+gsap.from(".box-2", {
+  right: "0",
+  opacity: 0,
+  scrollTrigger: {
+    trigger: ".box-2",
+    start: "top bottom",
+    scrub: true,
+  },
+});
+
+gsap.from(".box-3", {
+  left: "0",
+  opacity: 0,
+  scrollTrigger: {
+    trigger: ".box-3",
+    start: "top bottom",
+    scrub: true,
   },
 });
