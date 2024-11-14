a43b4c93244dd5a8fdea05807fbc2419903e0bfa
baillodlucie <122967677+baillodlucie@users.noreply.github.com>
2024-11-14 09:53:31 +0100

    résolution de me animations, réglages de détails, ajout des liens externes

--- src/js/main.js
+++ src/js/main.js
@@ -4,36 +4,6 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.from(".section-green-box-1", {
-  left: "-10",
-  opacity: 0,
-  scrollTrigger: {
-    trigger: ".section-green-box-1",
-    start: "top bottom",
-    scrub: true,
-  },
-});
-
-gsap.from(".section-green-box-2", {
-  right: "-20",
-  opacity: 0,
-  scrollTrigger: {
-    trigger: ".section-green-box-2",
-    start: "top bottom",
-    scrub: true,
-  },
-});
-
-gsap.from(".section-green-box-3", {
-  left: "0",
-  opacity: 0,
-  scrollTrigger: {
-    trigger: ".section-green-box-1",
-    start: "top bottom",
-    scrub: true,
-  },
-});
-
 gsap.to(".buy-button", {
   top: 20,
   scrollTrigger: {
@@ -43,14 +13,43 @@ gsap.to(".buy-button", {
   },
 });
 
-// gsap.to(".horizontal-scroll-container", {
-//   xPercent: -200,
-//   ease: "none",
-//   scrollTrigger: {
-//     trigger: ".horizontal-scroll",
-//     start: "top top",
-//     end: "+=300%",
-//     scrub: true,
-//     pin: true,
-//   },
-// });
+gsap
+  .timeline()
+  .to(".horizontal-scroll-container", {
+    xPercent: -200,
+    ease: "none",
+    scrollTrigger: {
+      trigger: ".horizontal-scroll",
+      start: "top top",
+      // end: "+=300%",
+      scrub: true,
+      pin: true,
+    },
+  })
+  .from(".section-green-box-1", {
+    left: "-10",
+    opacity: 0,
+    scrollTrigger: {
+      trigger: ".section-green-box-1",
+      start: "top bottom",
+      scrub: true,
+    },
+  })
+  .from(".section-green-box-2", {
+    right: "-20",
+    opacity: 0,
+    scrollTrigger: {
+      trigger: ".section-green-box-2",
+      start: "top bottom",
+      scrub: true,
+    },
+  })
+  .from(".section-green-box-3", {
+    left: "0",
+    opacity: 0,
+    scrollTrigger: {
+      trigger: ".section-green-box-3",
+      start: "top bottom",
+      scrub: true,
+    },
+  });
